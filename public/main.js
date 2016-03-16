bf1418dbfd8616d8a2ecb219297c715b18ac6256
ericweidman <ericweidman@gmail.com>
2016-03-16 11:55:46 -0400

    Displays links to files

--- public/main.js
+++ public/main.js
@@ -1,11 +1,12 @@
-function getFiles(filesData){
-    for (var i in filesData){
+function getFiles(filesData) {
+    for (var i in filesData) {
         var elem = $("<a>");
         elem.attr("href", "files/" + filesData[i].filename);
         elem.text(filesData[i].originalFilename);
         $("#fileList").append(elem);
         var elem2 = $("<br>");
-        $("fileList").append(elem2);
+        $("#fileList").append(elem2);
     }
 }
+
 $.get("/files", getFiles);
