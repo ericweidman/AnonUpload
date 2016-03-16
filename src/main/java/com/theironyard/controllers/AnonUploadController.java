36a26483c00dec669e3962f0f3623d0444b9fef9
ericweidman <ericweidman@gmail.com>
2016-03-16 11:34:53 -0400

    Make dirrrr

--- src/main/java/com/theironyard/controllers/AnonUploadController.java
+++ src/main/java/com/theironyard/controllers/AnonUploadController.java
@@ -2,7 +2,14 @@ package com.theironyard.controllers;
 
 import com.theironyard.services.AnonFileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by ericweidman on 3/16/16.
@@ -12,6 +19,12 @@ public class AnonUploadController {
     @Autowired
     AnonFileRepository files;
 
-
-
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public void upload(MultipartFile file, HttpServletResponse response) throws IOException {
+        File dir = new File("public/files");
+        dir.mkdirs();
+        File f = File.createTempFile("file", file.getOriginalFilename(), dir);
+        FileOutputStream fos = new FileOutputStream(f);
+        fos.write(file.getBytes());
+    }
 }
